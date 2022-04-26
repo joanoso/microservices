f1e84be605db954a41db71f1b9d0aee3c2aacaa9
joano <joan.manuel.docarmo@gmail.com>
2022-04-26 17:33:00 +0100

    first commit

---
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -0,0 +1,47 @@
+package com.amigoscode.customer;
+
+import com.amigoscode.amqp.RabbitMQMessageProducer;
+import com.amigoscode.clients.fraud.FraudCheckResponse;
+import com.amigoscode.clients.fraud.FraudClient;
+import com.amigoscode.clients.notification.NotificationRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final FraudClient fraudClient;
+    private final RabbitMQMessageProducer rabbitMQMessageProducer;
+
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+        customerRepository.saveAndFlush(customer);
+
+        // todo
+        FraudCheckResponse fraudsterCheckResponse =
+                fraudClient.isFraudster(customer.getId());
+
+        if (fraudsterCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+
+        NotificationRequest notificationRequest = new NotificationRequest(
+                customer.getId(),
+                customer.getEmail(),
+                String.format("Hi %s, welcome to Amigoscode...",
+                        customer.getFirstName())
+        );
+
+        rabbitMQMessageProducer.publish(notificationRequest,
+                "internal.exchange",
+                "internal.notification.routing-key");
+
+    }
+}
