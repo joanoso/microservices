81c38a4963cfce78aaa95d471c906b083cad5c6b
joano <joan.manuel.docarmo@gmail.com>
2022-04-29 18:57:19 +0100

    With Kubernates

--- clients/src/main/java/com/amigoscode/clients/fraud/FraudClient.java
+++ clients/src/main/java/com/amigoscode/clients/fraud/FraudClient.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
-        value = "fraud"
+        name = "fraud",
+        url = "${clients.fraud.url}"
+
 )
 public interface FraudClient {
     @GetMapping(path = "api/v1/fraud-check/{customerId}")
